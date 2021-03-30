c9fc5116dd8a55240c90a9b5357e56ef12b07f8e
BoazTurya <boazturya@gmail.com>
2021-03-30 00:14:15 -0500

    d

--- js/login.js
+++ js/login.js
@@ -1,11 +1,8 @@
-document.getElementById("submitButton").onclick= printInputs();
+const submitButton = document.getElementById("submitButton");
+submitButton.addEventListener("click",function(){
+   const emailbox = document.getElementById("emailbox").value;
+   const passbox = document.getElementById("passbox").value;
+   const urlbox = document.getElementById("urlbox").value;
 
-function getInputs(){
-   const form = document.getElementById("loginform");
-   return form.elements["email"].value;
-    
-}
-
-function printInputs(){
-    console.log(getInputs());
-}
+   alert("Email: "+emailbox+"\nPassword: "+passbox+"\nURL: "+urlbox);   
+});
