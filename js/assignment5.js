689973a13f765d44f5fd51fad871d0eed6e738f7
BoazTurya <boazturya@gmail.com>
2021-03-29 20:24:21 -0500

    l

--- js/assignment5.js
+++ js/assignment5.js
@@ -211,8 +211,6 @@ printFibo(length,starterA,starterB);
     15.	Using JavaScript and HTML and CSS, implement a webpage that displays a working, ticking counter Clock, 
     that counts/displays the current Date and time of the browser host, in the format: 2019-11-4 12:16:01  
 */
-
-
 console.log("Number 15:");
 
 function displayTime(){
