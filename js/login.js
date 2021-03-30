5b7f8bcf14f99261cfc689189823264ea690332d
BoazTurya <boazturya@gmail.com>
2021-03-30 16:12:11 -0500

    ll

--- js/login.js
+++ js/login.js
@@ -1,8 +1,13 @@
 const submitButton = document.getElementById("submitButton");
 submitButton.addEventListener("click",function(){
+      
+});
+
+document.getElementById("loginform").addEventListener("submit", function postform(event){
+   event.preventDefault();
    const emailbox = document.getElementById("emailbox").value;
    const passbox = document.getElementById("passbox").value;
    const urlbox = document.getElementById("urlbox").value;
-
-   console.log("Email: "+emailbox+"\nPassword: "+passbox+"\nURL: "+urlbox);   
+   const checked = document.getElementById("checkbox").checked;
+   console.log("Email: "+emailbox+"\nPassword: "+passbox+"\nURL: "+urlbox+"\nChecked Out? "+checked);
 });
