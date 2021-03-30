328075866372edc9f6738934a8a81174317a3a94
BoazTurya <boazturya@gmail.com>
2021-03-29 21:26:52 -0500

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,11 +2,10 @@ document.getElementById("submitButton").onclick= printInputs();
 
 function getInputs(){
    const form = document.getElementById("loginform");
-   return form.elements["emailbox"].value;
-    ;
+   return form.elements["email"].value;
+    
 }
 
 function printInputs(){
     console.log(getInputs());
 }
-document.getElementsByTagName("loginform").namedItem
