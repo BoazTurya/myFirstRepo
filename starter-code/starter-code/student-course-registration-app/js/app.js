07f5f3f77c105d9288d995f47d08d20a3739139d
BoazTurya <boazturya@gmail.com>
2021-04-03 11:05:35 -0500

    ;

---
+++ starter-code/starter-code/student-course-registration-app/js/app.js
@@ -0,0 +1,39 @@
+/**
+ * app.js
+ */
+"use strict";
+window.onload = function() {
+    let currentRowId = 2;
+    const formNewAccount = document.querySelector("#formCourseReg");
+    formNewAccount.addEventListener("submit", 
+        event => {
+            event.preventDefault();
+            const txtStudentId = document.querySelector("#txtStudentId");
+            const txtFullName = document.querySelector("#txtFullName");
+            const txtCourse = document.getElementById("courseName");
+            const strStudentId = txtStudentId.value;
+            const strFullName = txtFullName.value;
+            const stringCourse = txtCourse.value;
+            addNewRegistrationDataRow(strStudentId, strFullName,stringCourse);
+            txtStudentId.value = "";
+            txtFullName.value = "";
+            txtCourse.selectedIndex = 0;
+            txtStudentId.focus(); 
+    });
+    const addNewRegistrationDataRow = function(studentId, fullName, course) {
+        const tblRegistrations = document.querySelector("#tblRegistrations");
+        const newRow = tblRegistrations.insertRow(-1);
+        const newCellRowId = newRow.insertCell(0);
+        const strNewRowId = document.createTextNode(`${++currentRowId}.`);
+        newCellRowId.appendChild(strNewRowId);
+        const newCellStudentId = newRow.insertCell(1);
+        const strNewRowStudentId = document.createTextNode(`${studentId}`);
+        newCellStudentId.appendChild(strNewRowStudentId);
+        const newCellFullName = newRow.insertCell(2);
+        const strNewRowFullName = document.createTextNode(`${fullName}`);
+        newCellFullName.appendChild(strNewRowFullName);
+        const newCellCourse = newRow.insertCell(3);
+        const strNewRowCourse = document.createTextNode(`${course}`);
+        newCellCourse.appendChild(strNewRowCourse);
+    }
+}
