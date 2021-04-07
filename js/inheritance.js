60b95db432649d5bbc0134b5b48a37513bf17ee8
BoazTurya <boazturya@gmail.com>
2021-04-07 14:06:54 -0500

    dd

--- js/inheritance.js
+++ js/inheritance.js
@@ -1,20 +1,15 @@
 "use-strict"
 
-
 const person = {
     "name": "",
     'dateOfBirth': null,
     'getName': function(){
-                 return this.name;
-            },
+                 return this.name;},
     'setName': function(newName){
-                this.name = newName;
-    }
+                this.name = newName;}
 }
-
 const John = Object.create(person);
 John.setName("John");
 John.dateOfBirth = new Date(1998,11,10);
 
-console.log(`The person's name is ${John.getName()}`);
-console.log(`${John.getName()} was born on ${John.dateOfBirth.toDateString()}`);
+console.log(`The person's name is ${John.getName()}\n${John.getName()} was born on ${John.dateOfBirth.toDateString()}`);
