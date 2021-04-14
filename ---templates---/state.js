25e74c398a1e830590f3d7dc526760fa6dca7cef
BoazTurya <boazturya@gmail.com>
2021-04-14 08:31:25 -0500

    nn

---
+++ ---templates---/state.js
@@ -0,0 +1,8 @@
+/*
+SESSION
+HttpSession  session = request.getSession();
+session.setAttribute("userName", new Object());
+
+ServletContext app = request.getServletContext();
+app.setAttribute("afdsa",fafsfa)
+*/
